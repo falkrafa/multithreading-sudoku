b9efc15e208ad42ca8844804116998901ae99038
Rafael Falk <rafaelmartinsfalk2@gmail.com>
2023-03-22 09:17:40 -0300

    finished

--- rmf2/sudoku.c
+++ rmf2/sudoku.c
@@ -73,7 +73,6 @@ void *checandoValoresSub(void* args) {
 
 int main(int argc, char **argv) {
 
-    //vendo se passou parametros suficiente
     if (argv[1] == 0 || argc > 2) {
         printf("Invalid number of parameters\n");
         exit(0);
@@ -82,40 +81,35 @@ int main(int argc, char **argv) {
     FILE *input = fopen(argv[1], "r");
     char valorLinha[MAX_SIZE];
 
-    //vendo se o arquivo existe
     if(input == NULL){
         printf("Error to read file\n");
         exit(0);
     }
-    //Pega a primeira linha do arquivo e caso seja vazia retorna mensagem
+
     char *pegaLinha = fgets(valorLinha, MAX_SIZE, input);
 
     if(pegaLinha == NULL){
-        printf("File out of format\n");
+        printf("File out of format");
         exit(0);
     }
 
     int linhas, colunas, linhaSub, colunaSub;
     char *linha1 = strchr(valorLinha, 'x');
 
-    //aqui ele vai checar para saber se existe um primeiro valor e se esta escrito de maneira correta
     if(linha1 == NULL || sscanf(valorLinha, "%d x %d", &linhas, &colunas) != 2){
-        printf("File out of format\n");
+        printf("File out of format");
         exit(0);
     }
-    *linha1 = '\0'; //vai percorrer ate achar o x e vai parar
+    *linha1 = '\0';
+    linha1++;
 
-    linha1++; //continua depois do x
-    
-    //vai verificar se existe um valor depois do x
     if(sscanf(linha1, "%d", &colunas) != 1){
-        printf("File out of format\n");
+        printf("File out of format");
         exit(0);
     }
-    //pega a segunda linha e verificar para ver se esta escrita de maneira correta
     pegaLinha = fgets(valorLinha, MAX_SIZE, input);
     if(pegaLinha == NULL || sscanf(valorLinha, "%d x %d", &linhaSub, &colunaSub) != 2){
-        printf("File out of format\n");
+        printf("File out of format");
         exit(0);
     }
     if(linhas != colunas){
@@ -123,7 +117,7 @@ int main(int argc, char **argv) {
         exit(0);
     }
     if((linhaSub * colunaSub) != linhas){
-        printf("File out of format\n");
+        printf("File out of format");
         exit(0);
     }
     Sudoku sudoku = {linhas,colunas,linhaSub,colunaSub, NULL,0,0,0};
@@ -133,42 +127,40 @@ int main(int argc, char **argv) {
         sudoku.matriz[i] = malloc(linhas * sizeof(int *));
     }
 
-
-    // aqui vamos pegar os valores a partir da terceira linha
     for (int i = 0; i < linhas; i++) {
         pegaLinha = fgets(valorLinha, MAX_SIZE, input);
         if (pegaLinha == NULL) {
-            printf("File out format\n");
+            printf("File out format");
             exit(0);
         }
         char *tok = strtok(valorLinha, " ");
         for (int j = 0; j < colunas; j++) {
             if(tok == NULL){
-                printf("File out of format\n");
+                printf("File out of format");
                 exit(0);
             }
             if(sscanf(tok, "%d", &sudoku.matriz[i][j]) != 1){
-                printf("File out of format\n");
+                printf("File out of format");
                 exit(0);
             }
             if(sudoku.matriz[i][j]<=0 || sudoku.matriz[i][j] > linhas){
-                printf("File out of format\n");
+                printf("File out of format");
                 exit(0);
             }
             tok = strtok(NULL, " ");
         }
 
         if(tok != NULL){
-            printf("File out of format\n");
+            printf("File out of format");
             exit(0);
         }
     }
     int cont = 0;
-    char teste;
+    int teste;
     teste = fgetc(input);
     if(teste != EOF){
-    printf("File out of format\n");
-    exit(0);
+        printf("File out of format");
+        exit(0);
     }
 
     sudoku.linhas = linhas;
